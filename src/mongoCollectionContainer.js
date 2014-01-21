dbae8cd5592276053f3db571015e88c7061f9e93
David Beck <david@rotundasoftware.com>
2014-01-21 13:47:48 -0800

    added files

---
+++ src/mongoCollectionContainer.js
@@ -0,0 +1,70 @@
+var _ = require( 'underscore' );
+var async = require( "async" );
+var Class = require( "./class" );
+var BaseContainer = require( "./baseContainer" );
+
+module.exports = BaseContainer.extend( {
+	initialize : function( options ) {
+		this._collection = options.collection;
+
+		this._super( arguments );
+	},
+
+	stuff : function( callback ) {
+		var _this = this;
+		var recordsById = {};
+
+		async.each( this._manifest, function( thisSelector, callback ) {
+			thisSelector = _.extend( {
+				fields : [],
+				where : {},
+				filters : [],
+				skip : 0,
+				limit : 0
+			}, thisSelector );
+
+			var allConditions = [];
+			if( thisSelector.where ) allConditions.push( thisSelector.where );
+
+			var projection = {};
+
+			var mongoQuery = allConditions.length > 1 ? { $and : allConditions } : _.first( allConditions );
+			if( thisSelector.fields && thisSelector.fields !== "*" ) {
+				thisSelector.fields = _.union( thisSelector.fields, "_id" ); // _id field is mandatory
+				
+				_.each( thisSelector.fields, function( thisField ) {
+					projection[ thisField ] = true;
+				} );
+			}
+
+			var cursor = _this._collection.find( mongoQuery, projection );
+			cursor.toArray( function( err, records ) {
+				if( err ) return next( err );
+
+				// apply filter functions supplied in thisSelector.filters. allows for pseudo joins
+				_.each( thisSelector.filters, function( thisFilter ) {
+					records = _.filter( records, thisFilter );
+				} );
+
+				// apply skip and limit, now that filters have been applied
+				if( thisSelector.limit || thisSelector.skip )
+					records = records.splice( thisSelector.skip, thisSelector.limit );
+
+				_.each( records, function( thisRecord ) {
+					var recordId = thisRecord._id;
+
+					recordsById[ recordId ] = recordsById[ recordId ] || {};
+					_.extend( recordsById[ recordId ], thisRecord );
+				} );
+
+				callback();
+			} );
+		}, function( err ) {
+			if( err ) return callback( err );
+
+			// now all records are in recordsById, but we want the final payload to be an array
+			var containerContents = _.values( recordsById );
+			callback( null, containerContents );
+		} );
+	}
+} );
