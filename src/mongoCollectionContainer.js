60408d3be171cd6975f7b4cd428b09deb56e3a7e
David Beck <david@rotundasoftware.com>
2014-01-22 18:09:05 -0800

    code cleanup, comments

--- src/mongoCollectionContainer.js
+++ src/mongoCollectionContainer.js
@@ -1,3 +1,10 @@
+/*
+ * Steamer Mongo Collection Container v0.2.0
+ * Copyright (c)2014 Rotunda Software, LLC.
+ * Distributed under MIT license
+ * http://github.com/rotundasoftware/steamer
+*/
+
 var _ = require( 'underscore' );
 var async = require( "async" );
 
@@ -6,7 +13,8 @@ module.exports = function( options ) {
 	this._selectors = [];
 
 	this.add = function( item ) {
-		this._selectors.push( item );
+		// Supports adding either a single selector object or an array of selector objects.
+		this._selectors = this._selectors.concat( item );
 	};
 
 	this.reset = function() {
@@ -32,7 +40,7 @@ module.exports = function( options ) {
 			var projection = {};
 
 			var mongoQuery = allConditions.length > 1 ? { $and : allConditions } : _.first( allConditions );
-			if( thisSelector.fields && thisSelector.fields !== "*" ) {
+			if( thisSelector.fields !== "*" ) {
 				thisSelector.fields = _.union( thisSelector.fields, "_id" ); // _id field is mandatory
 				
 				_.each( thisSelector.fields, function( thisField ) {
@@ -41,14 +49,11 @@ module.exports = function( options ) {
 			}
 
 			var cursor = _this._collection.find( mongoQuery, projection );
-			if( thisSelector.sort ) cursor.sort( thisSelector.sort );
+			if( thisSelector.skip ) cursor.skip( thisSelector.skip );
+			if( thisSelector.limit ) cursor.limit( thisSelector.limit );
 			
 			cursor.toArray( function( err, records ) {
 				if( err ) return next( err );
-
-				// apply skip and limit, now that filters have been applied
-				if( thisSelector.limit || thisSelector.skip )
-					records = records.splice( thisSelector.skip, thisSelector.limit );
 
 				_.each( records, function( thisRecord ) {
 					var recordId = thisRecord._id;
@@ -61,8 +66,8 @@ module.exports = function( options ) {
 			} );
 		}, function( err ) {
 			if( err ) return callback( err );
-
-			// now all records are in recordsById, but we want the final payload to be an array
+			
+			// now all records are in recordsById, but we want the final payload to be an array, not a hash
 			var containerContents = _.values( recordsById );
 			callback( null, containerContents );
 		} );
