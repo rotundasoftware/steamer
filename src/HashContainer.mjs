a5629504cefacd81521b9f5b5fb2c225affcd3b8
Nicolas Ledesma <nicolas@rotundasoftware.com>
2024-05-09 16:10:06 -0300

    Fix typo in HashContainer

--- src/HashContainer.mjs
+++ src/HashContainer.mjs
@@ -5,7 +5,7 @@ export default class {
     #keys = [];
 
     constructor( hash = {} ) {
-        assertType( { containers }, 'object' );
+        assertType( { hash }, 'object' );
         
         this.#hash = hash;
     }
